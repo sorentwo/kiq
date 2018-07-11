b72fd7d78d79d5f78ccf06be57e82af600578cd8
Parker Selbert <parker@sorentwo.com>
2018-07-11 12:19:51 -0400

    Workers only generate jobs for enqueueing
    As part of the restructuring to top level setup this removes
    `perform_async` and `perform_in` from the `Worker` module. Instead the
    worker knows how to generate jobs for itself.

--- lib/kiq/worker.ex
+++ lib/kiq/worker.ex
@@ -7,30 +7,24 @@ defmodule Kiq.Worker do
   enqueued with the `Kiq.Job`.
   """
 
-  alias Kiq.{Client, Job, Timestamp}
+  alias Kiq.Job
 
   @type args :: list(any())
   @type opts :: [queue: binary(), retry: boolean()]
-  @type client :: GenServer.server()
-  @type seconds :: pos_integer()
-  @type on_enqueue() :: {:ok, Job.t()} | {:error, Exception.t()}
+
+  @doc """
+  Build a job for this worker using all default options.
+
+  Any additional arguments that are provided will be merged into the job.
+  """
+  @callback new(args :: args()) :: Job.t()
 
   @doc """
   The `perform/1` function is called with the enqueued arguments.
+
   The return value is not important.
   """
-  @callback perform(args()) :: any()
-
-  @doc """
-  Enqueues a job to be performed for the worker as soon as possible.
-  """
-  @callback perform_async(client(), args()) :: on_enqueue()
-
-  @doc """
-  Enqueues a job N seconds in the future. The job will be performed on or after
-  that time, it isn't an exact guarantee.
-  """
-  @callback perform_in(client(), seconds(), args()) :: on_enqueue()
+  @callback perform(args :: args()) :: any()
 
   defmacro __using__(opts) do
     opts = Keyword.put_new(opts, :queue, "default")
@@ -41,42 +35,25 @@ defmodule Kiq.Worker do
       @behaviour Worker
 
       @impl Worker
+      def new(args) when is_list(args) do
+        Worker.new(__MODULE__, args, unquote(opts))
+      end
+
+      @impl Worker
       def perform(args) when is_list(args) do
         :ok
       end
 
-      @impl Worker
-      def perform_async(client, args) when is_list(args) do
-        Worker.perform_async(client, __MODULE__, args, unquote(opts))
-      end
-
-      @impl Worker
-      def perform_in(client, seconds, args) when is_integer(seconds) and is_list(args) do
-        Worker.perform_in(client, __MODULE__, seconds, args, unquote(opts))
-      end
-
-      defoverridable perform: 1, perform_async: 2, perform_in: 3
+      defoverridable new: 1, perform: 1
     end
   end
 
   @doc false
-  @spec perform_async(client(), module(), args(), opts()) :: on_enqueue()
-  def perform_async(client, module, args, opts) do
-    enqueue(client, %{class: module, args: args}, opts)
-  end
-
-  @doc false
-  @spec perform_in(client(), module(), seconds(), args(), opts()) :: on_enqueue()
-  def perform_in(client, module, seconds, args, opts) do
-    enqueue(client, %{class: module, args: args, at: Timestamp.unix_in(seconds)}, opts)
-  end
-
-  defp enqueue(client, job_map, opts) do
-    job =
-      opts
-      |> Enum.into(job_map)
-      |> Job.new()
-
-    Client.enqueue(client, job)
+  @spec new(module(), map() | Keyword.t(), opts()) :: Job.t()
+  def new(module, args, opts) do
+    opts
+    |> Keyword.put(:args, args)
+    |> Keyword.put(:class, module)
+    |> Job.new()
   end
 end
--- test/kiq/worker_test.exs
+++ test/kiq/worker_test.exs
@@ -1,68 +1,43 @@
 defmodule Kiq.WorkerTest do
   use ExUnit.Case, async: true
 
-  alias Kiq.{Job, Worker, Timestamp}
+  alias Kiq.{Job, Worker}
 
-  defmodule MyClient do
-    use GenServer
+  defmodule BasicWorker do
+    use Worker
+  end
 
-    def start_link(opts) do
-      opts = Keyword.put_new(opts, :test_pid, self())
+  defmodule CustomWorker do
+    use Worker, queue: "special", retry: 5
 
-      GenServer.start_link(__MODULE__, opts)
-    end
-
-    def init(opts) do
-      {:ok, opts}
-    end
-
-    def handle_call({:enqueue, job}, _from, state) do
-      {:reply, {:ok, job}, state}
-    end
-
-    def handle_call({:enqueue_at, job, _set}, _from, state) do
-      {:reply, {:ok, job}, state}
+    @impl Worker
+    def perform([a, b]) do
+      a + b
     end
   end
 
-  defmodule MyWorker do
-    use Worker, queue: "special", retry: false
-  end
+  describe "new/1" do
+    test "workers default to using the 'default' queue" do
+      assert %Job{queue: "default"} = BasicWorker.new([])
+    end
 
-  setup do
-    {:ok, pid} = start_supervised({MyClient, []})
+    test "generating a new job with the stored options" do
+      assert %Job{} = job = CustomWorker.new([1, 2])
 
-    {:ok, pid: pid}
+      assert job.args == [1, 2]
+      assert job.class == "Elixir.Kiq.WorkerTest.CustomWorker"
+      assert job.queue == "special"
+      assert job.retry == 5
+    end
   end
 
   describe "perform/1" do
     test "a default implementation is provided" do
-      assert :ok = MyWorker.perform([])
+      assert :ok = BasicWorker.perform([])
     end
-  end
 
-  describe "perform_async/1" do
-    test "enqueueing a job for the worker module", %{pid: pid} do
-      assert {:ok, %Job{} = job} = MyWorker.perform_async(pid, [1, 2])
-
-      assert job.args == [1, 2]
-      assert job.class == to_string(MyWorker)
-      assert job.queue == "special"
-      refute job.retry
-    end
-  end
-
-  describe "perform_in/2" do
-    test "enqueueing a job in the future", %{pid: pid} do
-      assert {:ok, %Job{} = job} = MyWorker.perform_in(pid, 300, [1, 2])
-
-      assert job.args == [1, 2]
-      assert job.class == to_string(MyWorker)
-      assert job.queue == "special"
-      refute job.retry
-      refute job.enqueued_at
-
-      assert_in_delta job.at, Timestamp.unix_in(300), 2
+    test "the implementation may be overridden" do
+      assert 5 == CustomWorker.perform([2, 3])
     end
   end
 end
